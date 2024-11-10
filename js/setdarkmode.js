fe0ce35f92b34c924a462eedd7f574919f4a46f1
Milo <149924738+actuallymilo@users.noreply.github.com>
2024-11-09 22:06:28 -0600

    make darkMode cookie accessible across all boykisser.com subdomains.

--- js/setdarkmode.js
+++ js/setdarkmode.js
@@ -13,8 +13,8 @@ function setDarkMode(isDark) {
 
 function savePreference(isDark) {
     var date = new Date();
-    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000)); // 1 year
-    document.cookie = 'darkMode=' + isDark + '; expires=' + date.toUTCString() + '; path=/';
+    date.setTime(date.getTime() + (365 * 24 * 60 * 60 * 1000));
+    document.cookie = 'darkMode=' + isDark + '; expires=' + date.toUTCString() + '; path=/; domain=.boykisser.com';
 }
 
 function getPreference() {
